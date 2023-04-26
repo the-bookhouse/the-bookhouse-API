1bea6d4515de1266086eef18692bda93a7bd7b36
yurilopesm <yurilopesm@hotmail.com>
2023-04-26 19:30:01 -0300

    mongodb connection failed- but improved

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`))
