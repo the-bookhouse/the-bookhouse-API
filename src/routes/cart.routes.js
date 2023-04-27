33507bb3954c74ec1099ecbacb72c80dab1f622c
yurilopesm <yurilopesm@hotmail.com>
2023-04-27 10:11:16 -0300

    cart routes and controller connected

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { authValidate } from "../middlewares/auth.middleware.js"
+import { getProducts, postProducts } from "../controllers/cart.controller.js"
 
 const cartRouter = Router()
 
@@ -7,11 +8,11 @@ cartRouter.use(authValidate)
 
 //usado pela pagina de produtos para postar um produto na collection 'cart'
 //o objeto postado deve ter o id do usuario, o id do produto, e a quantidade 
-cartRouter.post("/cart")
+cartRouter.post("/cart", postProducts)
 
 //usado pela pagina de carrinho para pegar da collection carrinho todos os produtos adicionados pelo id do user
 //deve retornar uma lista contendo os produtos selecionados e a quantidade
-cartRouter.get("/cart")
+cartRouter.get("/cart", getProducts)
 
 
 export default cartRouter
