ab01ca8213fa914e15187fb89c0b9ae51c5fc18b
yurilopesm <yurilopesm@hotmail.com>
2023-04-29 18:49:37 -0300

    postSale function implemented

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -6,11 +6,20 @@ export async function postProducts(req, res) {
 }
 
 export async function getProducts(req, res) {
-    const {user, session} = res.locals
+    const user = res.locals.user
     try {
         const products = await db.collection('cart').find({ userId: user.userId}).toArray()
         if(!products) return res.send([])
         res.send(products)
+    }catch(err) {
+        res.status(500).send(err.message)
+    }
+}
+export async function postSale(req, res) {
+    const {userId, value} = req.body
+    try {
+        await db.collection('sales').insertOne({userId, value})
+        return res.sendStatus(201)
     }catch(err) {
         res.status(500).send(err.message)
     }
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { authValidate } from "../middlewares/auth.middleware.js"
-import { getProducts, postProducts } from "../controllers/cart.controller.js"
+import { getProducts, postProducts, postSale } from "../controllers/cart.controller.js"
 
 const cartRouter = Router()
 
@@ -13,6 +13,8 @@ cartRouter.post("/cart", postProducts)
 //usado pela pagina de carrinho para pegar da collection carrinho todos os produtos adicionados pelo id do user
 //deve retornar uma lista contendo os produtos selecionados e a quantidade
 cartRouter.get("/cart", getProducts)
+
+cartRouter.post("/checkout", postSale)
 
 
 export default cartRouter
