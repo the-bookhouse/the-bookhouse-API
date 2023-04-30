536d3c134a01b0cc053716c4b63f6554e51e5344
yurilopesm <yurilopesm@hotmail.com>
2023-04-30 15:49:44 -0300

    getProducts implemented

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,21 +1,27 @@
+import { ObjectId } from 'mongodb'
 import { db } from '../database/database.connection.js'
 
 export async function getProducts(req, res) {
     const session = res.locals.session
     try {
-        const products = await db.collection('cart').find({ userId: session.userId}).toArray()
-        if(!products) return res.send([])
-        res.send(products)
-    }catch(err) {
+        const products = await db.collection('cart').find({ userId: new ObjectId(session.userId) }).toArray()
+        const list = []
+        for(let i = 0; i < products.length; i++) {
+            const info = await db.collection('products').findOne({_id: new ObjectId(products[i].productId)})
+            list.push(info)
+        }
+        res.send(list)
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
+
 export async function postSale(req, res) {
-    const {userId, value} = req.body
+    const { userId, value } = req.body
     try {
-        await db.collection('sales').insertOne({userId, value})
+        await db.collection('sales').insertOne({ userId, value })
         return res.sendStatus(201)
-    }catch(err) {
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
