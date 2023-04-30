48cb362f823832741e12ee62e7acfc5c8af54568
yurilopesm <yurilopesm@hotmail.com>
2023-04-30 12:05:24 -0300

    cart get and checkout post routes available

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,14 +1,9 @@
 import { db } from '../database/database.connection.js'
 
-export async function postProducts(req, res) {
-    //usado pela pagina de produtos para postar um produto na collection 'cart'
-    //o objeto postado deve ter o id do usuario, o id do produto, e a quantidade
-}
-
 export async function getProducts(req, res) {
-    const user = res.locals.user
+    const session = res.locals.session
     try {
-        const products = await db.collection('cart').find({ userId: user.userId}).toArray()
+        const products = await db.collection('cart').find({ userId: session.userId}).toArray()
         if(!products) return res.send([])
         res.send(products)
     }catch(err) {
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,14 +1,10 @@
 import { Router } from "express"
 import { authValidate } from "../middlewares/auth.middleware.js"
-import { getProducts, postProducts, postSale } from "../controllers/cart.controller.js"
+import { getProducts, postSale } from "../controllers/cart.controller.js"
 
 const cartRouter = Router()
 
 cartRouter.use(authValidate)
-
-//usado pela pagina de produtos para postar um produto na collection 'cart'
-//o objeto postado deve ter o id do usuario, o id do produto, e a quantidade 
-cartRouter.post("/cart", postProducts)
 
 //usado pela pagina de carrinho para pegar da collection carrinho todos os produtos adicionados pelo id do user
 //deve retornar uma lista contendo os produtos selecionados e a quantidade
