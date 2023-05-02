881963ec239f3e5f780c9172a58eb5509b7f3f01
yurilopesm <yurilopesm@hotmail.com>
2023-05-02 10:42:15 -0300

    removing unecessary sentences

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,11 +5,7 @@ import { getProducts, postSale } from "../controllers/cart.controller.js"
 const cartRouter = Router()
 
 cartRouter.use(authValidate)
-
-//usado pela pagina de carrinho para pegar da collection carrinho todos os produtos adicionados pelo id do user
-//deve retornar uma lista contendo os produtos selecionados e a quantidade
 cartRouter.get("/cart", getProducts)
-
 cartRouter.post("/checkout", postSale)
 
 
